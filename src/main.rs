c8a9b4e308127edc79c5f8dfb23a631bdd00e950
Num0Programmer <asajh4@gmail.com>
2023-05-19 17:29:59 -0700

    Implemented a function which has the job of taking a window and a Vulkan entry point to create a Vulkan instance. Added to App structure's  function to take care of setup and creating this Vulkan instance for the application.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@
     clippy::unnecessary_wraps
 )]
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use log::*;
+use vulkanalia::loader::{LibloadingLoader, LIBRARY};
+use vulkanalia::window as vk_window;
+use vulkanalia::prelude::v1_0::*;
 use winit::dpi::LogicalSize;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::{ControlFlow, EventLoop};
@@ -46,16 +50,44 @@ fn main() -> Result<()>
     });
 }
 
+unsafe fn create_instance(window: &Window, entry: &Entry) -> Result<Instance>
+{
+    let application_info = vk::ApplicationInfo::builder()
+        .application_name(b"Vulkan Tutorial\0")
+        .application_version(vk::make_version(1, 0, 0))
+        .engine_name(b"No Engine\0")
+        .engine_version(vk::make_version(1, 0, 0))
+        .api_version(vk::make_version(1, 0, 0));
+
+    let extensions = vk_window::get_required_instance_extensions(window)
+        .iter()
+        .map(|e| e.as_ptr())
+        .collect::<Vec<_>>();
+
+    let info = vk::InstanceCreateInfo::builder()
+        .application_info(&application_info)
+        .enabled_extension_names(&extensions);
+
+    Ok(entry.create_instance(&info, None)?)
+}
+
 /// vulkan application
 #[derive(Clone, Debug)]
-struct App {}
+struct App
+{
+    entry: Entry,
+    instance: Instance
+}
 
 impl App
 {
     /// creates our vulkan app
     unsafe fn create(window: &Window) -> Result<Self>
     {
-        Ok(Self {})
+        let loader = LibloadingLoader::new(LIBRARY)?;
+        let entry = Entry::new(loader).map_err(|b| anyhow!("{}", b))?;
+        let instance = create_instance(window, &entry)?;
+        Ok(Self { entry, instance })
     }
 
     /// renders a frame from our vulkan application
