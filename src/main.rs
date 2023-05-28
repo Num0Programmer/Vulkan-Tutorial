5fc43cfdb450c0c805053882eaa7550549141c49
Num0Programmer <asajh4@gmail.com>
2023-05-28 16:58:20 -0700

    Finished implementation to create a logical device and the queue(s) to submit commands to the physical graphics device.

--- src/main.rs
+++ src/main.rs
@@ -152,6 +152,51 @@ unsafe fn create_instance(
     Ok(instance)
 }
 
+unsafe fn create_logical_device(
+    entry: &Entry,
+    instance: &Instance,
+    data: &mut AppData
+) -> Result<Device>
+{
+    let indices = QueueFamilyIndices::get(instance, data, data.physical_device)?;
+
+    let queue_priorities = &[1.0];
+    let queue_info = vk::DeviceQueueCreateInfo::builder()
+        .queue_family_index(indices.graphics)
+        .queue_priorities(queue_priorities);
+
+    let layers = if VALIDATION_ENABLED
+    {
+        vec![VALIDATION_LAYER.as_ptr()]
+    }
+    else
+    {
+        vec![]
+    };
+
+    let mut extensions = vec![];
+    // required by Vulkan SDK on macOS since 1.3.216
+    if cfg!(target_os = "macos") && entry.version()? >= PORTABILITY_MACOS_VERSION
+    {
+        extensions.push(vk::KHR_PORTABILITY_SUBSET_EXTENSION.name.as_ptr());
+    }
+
+    let features = vk::PhysicalDeviceFeatures::builder();
+    
+    let queue_infos = &[queue_info];
+    let info = vk::DeviceCreateInfo::builder()
+        .queue_create_infos(queue_infos)
+        .enabled_layer_names(&layers)
+        .enabled_extension_names(&extensions)
+        .enabled_features(&features);
+
+    let device = instance.create_device(data.physical_device, &info, None)?;
+
+    data.graphics_queue = device.get_device_queue(indices.graphics, 0);
+
+    Ok(device)
+}
+
 unsafe fn pick_physical_device(
     instance: &Instance,
     data: &mut AppData
@@ -212,7 +257,8 @@ struct App
 {
     entry: Entry,
     instance: Instance,
-    data: AppData
+    data: AppData,
+    device: Device
 }
 
 impl App
@@ -225,7 +271,8 @@ impl App
         let mut data = AppData::default();
         let instance = create_instance(window, &entry, &mut data)?;
         pick_physical_device(&instance, &mut data)?;
-        Ok(Self { entry, instance, data })
+        let device = create_logical_device(&entry, &instance, &mut data)?;
+        Ok(Self { entry, instance, data, device })
     }
 
     /// renders a frame from our vulkan application
@@ -237,6 +284,7 @@ impl App
     /// destroys our vulkan application
     unsafe fn destroy(&mut self)
     {
+        self.device.destroy_device(None);
         if VALIDATION_ENABLED
         {
             self.instance.destroy_debug_utils_messenger_ext(
@@ -252,7 +300,8 @@ impl App
 struct AppData
 {
     messenger: vk::DebugUtilsMessengerEXT,
-    physical_device: vk::PhysicalDevice
+    physical_device: vk::PhysicalDevice,
+    graphics_queue: vk::Queue
 }
 
 #[derive(Debug, Error)]
